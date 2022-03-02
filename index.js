0e841e446cf580cbc3be37943023498c1338f640
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-02 21:12:30 +0800

    add create new proj command

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ import { createRequire } from "module";
 import colors from "colors";
 const require = createRequire(import.meta.url);
 const version = require("./package.json").version;
+import generateProject from "./src/generator.js";
 
 const flag = argv[2];
 if (flag === "-v" || flag === "--version") {
   console.log("Ngenly CLI version: ".red + version.cyan);
   console.log("Created by Mart Anthony Salazar".red);
   console.log("https://github.com/mart-anthony-stark".cyan);
+} else if (flag === "new") {
+  generateProject();
 } else {
   console.log(`Unknown flag: ${flag}`.red);
-  console.log("unnamed-cli [flag]".cyan);
+  console.log("ngenly [flag]".cyan);
   console.log("-v             returns cli version".blue);
   console.log("-version       returns cli version".blue);
+  console.log("-new           generates a new project".blue);
 }
