6d340a8de2f9c51d78947f9a627b984bc9ab1193
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-02 21:48:23 +0800

    add w || w/o auth methods

--- src/templates.js
+++ src/templates.js
@@ -2,8 +2,11 @@ export default {
   javascript: {
     express: {
       none: "https://github.com/mart-anthony-stark/Node-boilerplates#expressjs",
-      "mongodb (mongoose)":
-        "https://github.com/mart-anthony-stark/Node-boilerplates#express-mongoose",
+      "mongodb (mongoose)": {
+        auth: "https://github.com/mart-anthony-stark/Node-boilerplates#expressjs-mongoose-auth",
+        noauth:
+          "https://github.com/mart-anthony-stark/Node-boilerplates#express-mongoose",
+      },
     },
   },
   typescript: {
