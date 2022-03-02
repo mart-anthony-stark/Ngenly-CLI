5dd19fc45790102bf6400708a2fc808fb9ec8819
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-02 20:53:25 +0800

    add cli arg checker

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { argv } from "process";
+import { createRequire } from "module";
+import colors from "colors";
+const require = createRequire(import.meta.url);
+const version = require("./package.json").version;
+
+const flag = argv[2];
+if (flag === "-v" || flag === "--version") {
+  console.log("Ngenly CLI version: ".red + version.cyan);
+  console.log("Created by Mart Anthony Salazar".red);
+  console.log("https://github.com/mart-anthony-stark".cyan);
+} else {
+  console.log(`Unknown flag: ${flag}`.red);
+  console.log("unnamed-cli [flag]".cyan);
+  console.log("-v             returns cli version".blue);
+  console.log("-version       returns cli version".blue);
+}
