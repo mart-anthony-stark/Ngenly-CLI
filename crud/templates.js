b715b8c3471f2ce20438917f9b5143db16f84ed5
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-29 20:04:06 +0800

    Add: express route template

--- crud/templates.js
+++ crud/templates.js
@@ -1,8 +1,15 @@
 export const routeTemplate = (name) => {
   return `
-const ${name}Model = require('../models/${
-    name.charAt(0).toUpperCase() + name.slice(1)
-  }.route.js')
+const router = require("express").Router();
+const ${name.toLowerCase()}Controller = require("../controller/${name.toLowerCase()}.controller");
+
+router.get("/${name.toLowerCase()}", ${name.toLowerCase()}Controller.getAll);
+router.get("/${name.toLowerCase()}/:id", ${name.toLowerCase()}Controller.getOne);
+router.post("/${name.toLowerCase()}", ${name.toLowerCase()}Controller.create);
+router.put("/${name.toLowerCase()}/:id", ${name.toLowerCase()}Controller.updateOne);
+router.delete("/${name.toLowerCase()}/:id", ${name.toLowerCase()}Controller.deleteOne);
+
+module.exports = router;
 
 `;
 };
