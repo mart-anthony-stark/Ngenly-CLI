ec9cc47ca44b81133ae6206fe2716b019bc67662
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-29 21:07:34 +0800

    Fixed: code design pattern for valid library selection

--- crud/index.js
+++ crud/index.js
@@ -1,12 +1,37 @@
 import fs from "fs";
 import path from "path";
-import { expressRouteTemplate, expressModelTemplate, expressControllerTemplate } from "./templates.js";
+import {
+  expressRouteTemplate,
+  expressModelTemplate,
+  expressControllerTemplate,
+} from "./templates-js.js";
+import colors from "colors";
+
+const templates = {
+  expressjs: {
+    route: expressRouteTemplate,
+    model: expressModelTemplate,
+    controller: expressControllerTemplate,
+  },
+};
 
 const __dirname = path.resolve(path.dirname(""));
 const generateCRUD = (library, name) => {
-  generateFile("route", name, expressRouteTemplate(name));
-  generateFile("model", name, expressModelTemplate(name));
-  generateFile("controller", name, expressControllerTemplate(name));
+  // CHECKS IF CRUD TEMPLATES FOR LIBRARY SELECTED IS VALID
+  if (!templates[library]) {
+    console.log("Invalid library selection".red);
+    console.log("Available library templates:".green);
+    console.log("\t- expressjs".blue);
+    console.log("\t- expressts".blue);
+    return;
+  }
+
+  const routeTemplate = templates[library]["route"];
+  const modelTemplate = templates[library]["model"];
+  const controllerTemplate = templates[library]["controller"];
+  generateFile("route", name, routeTemplate(name));
+  generateFile("model", name, modelTemplate(name));
+  generateFile("controller", name, controllerTemplate(name));
 };
 
 const generateFile = (dir, name, content) => {
