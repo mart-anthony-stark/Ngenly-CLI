577d85f2b5289d8faeb4de3981002fcf5d3d7e9c
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-29 20:48:59 +0800

    Add: create controller template

--- crud/templates.js
+++ crud/templates.js
@@ -6,13 +6,11 @@ const ${name.toLowerCase()}Controller = require("../controller/${name.toLowerCas
 
 router.get("/${name.toLowerCase()}", ${name.toLowerCase()}Controller.getAll);
 router.get("/${name.toLowerCase()}/:id", ${name.toLowerCase()}Controller.getOne);
-router.post("/${name.toLowerCase()}", ${name.toLowerCase()}Controller.create);
+router.post("/${name.toLowerCase()}", ${name.toLowerCase()}Controller.createOne);
 router.put("/${name.toLowerCase()}/:id", ${name.toLowerCase()}Controller.updateOne);
 router.delete("/${name.toLowerCase()}/:id", ${name.toLowerCase()}Controller.deleteOne);
 
-module.exports = router;
-
-`;
+module.exports = router;`;
 };
 
 // MONGOOSE MODEL TEMPLATE FOR EXPRESS.JS
@@ -22,45 +20,45 @@ export const expressModelTemplate = (name) => {
 const ${
     name.charAt(0).toUpperCase() + name.slice(1)
   }Schema = new mongoose.Schema(
-{
-  // title: {
-  //   type: String,
-  //   required: true,
-  // },
-  // body: {
-  //   type: String,
-  //   required: true,
-  // },
-},
-{ timestamps: true }
-);
+    {
+      // title: {
+      //   type: String,
+      //   required: true,
+      // },
+      // body: {
+      //   type: String,
+      //   required: true,
+      // },
+    },
+    { timestamps: true }
+  );
 
 module.exports = mongoose.model("${
     name.charAt(0).toUpperCase() + name.slice(1)
-  }", ${name.charAt(0).toUpperCase() + name.slice(1)}Schema);
-  `;
+  }", ${name.charAt(0).toUpperCase() + name.slice(1)}Schema);`;
 };
 
 // CONTROLLER TEMPLATE FOR EXPRESS.JS
 export const expressControllerTemplate = (name) => {
-  return `
-const ${name} = require("../models/${name}.model");
+  const capitalName = name.charAt(0).toUpperCase() + name.slice(1);
+  return `const ${name} = require("../models/${name}.model");
 
 module.exports = {
   // GET ALL DATA
   getAll: async (req, res) => {
-    const ${name.toLowerCase()}s = await ${
-    name.charAt(0).toUpperCase() + name.slice(1)
-  }.find();
+    const ${name.toLowerCase()}s = await ${capitalName}.find();
     res.send(${name.toLowerCase()}s);
   },
   // GET ONE DATA
   getOne: async (req, res) => {
-    const ${name.toLowerCase()} = await ${
-    name.charAt(0).toUpperCase() + name.slice(1)
-  }.findOne({ _id: req.params.id });
+    const ${name.toLowerCase()} = await ${capitalName}.findOne({ _id: req.params.id });
     res.send(${name.toLowerCase()})
   },
+  createOne: async (req,res) => {
+    const new${capitalName} = new ${capitalName}(req.body)
+    await new${capitalName}.save()
+    res.send(new${capitalName})
+  }
 };
 `;
 };
