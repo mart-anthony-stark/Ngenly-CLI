f11f9ffe677bc6369e93803f3f9cfdce687f12ff
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-01 11:16:33 +0800

    Refactor: nested if else into mapped functions

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 
 import { argv } from "process";
 import { createRequire } from "module";
-import colors from "colors";
 const require = createRequire(import.meta.url);
 const version = require("./package.json").version;
 import generateProject from "./src/generator.js";
-// const generateCRUD = require('./crud/index.js')
-import generateCRUD from "./crud/index.js";
-import { generateMenu } from "./crud/menu.js";
+import { generateCmd, help, versionCmd } from "./helper/commands.js";
 
 const greeting = ` _   _                  _       
 | \ | |                | |      
@@ -19,51 +16,23 @@ const greeting = ` _   _                  _
         __/ |              __/ |
        |___/              |___/ `;
 
-const flag = argv[2];
+const flag = argv[2].toLowerCase();
 
 console.log(greeting.yellow);
 
-if (flag === "-v" || flag === "--version") {
-  console.log("Ngenly CLI version: ".red + version.cyan);
-  console.log("Created by Mart Anthony Salazar".red);
-  console.log("https://github.com/mart-anthony-stark".cyan);
-} else if (flag === "new") {
-  // Generating new project
-  generateProject();
-} else if (flag === "-g" || flag === "generate") {
-  if (argv.length == 3) {
-    // Generate menu
-    generateMenu();
-  } else if (!argv[3] || !argv[4] || !argv[5]) {
-    console.log("Invalid arguments.".red);
-    console.log(
-      "Usage: ngenly -g [crud] [library: express | fastify] [route_name]".yellow
-    );
-  } else {
-    if (argv[3] == "crud") {
-      generateCRUD(argv[4], argv[5]);
-    }
-  }
+const commandExecutions = {
+  version: versionCmd,
+  "-v": versionCmd,
+  new: generateProject,
+  "-n": generateProject,
+  "-g": generateCmd,
+  "-h": help,
+  help: help,
+};
+
+const execute = commandExecutions[flag];
+if (!execute) {
+  help();
 } else {
-  console.log(`Unknown flag: ${flag}`.red);
-  console.log("\nUsage: ".yellow + "ngenly [flag]".cyan);
-  console.log("Commands: ".yellow);
-  console.log(
-    "\tngenly new".magenta + "                           generates a new project".blue
-  );
-  console.log(
-    "\tngenly [-g | generate]".magenta +
-      "               starts generating project or crud using menu".blue
-  );
-  console.log(
-    "\tngenly -v".magenta + "                            returns cli version".blue
-  );
-  console.log(
-    "\tngenly --version".magenta +
-      "                     returns cli version".blue
-  );
-  console.log(
-    "\tngenly -g [crud] [library] [name]".magenta +
-      "    generates crud files (Model, Controller, Router)".blue
-  );
+  execute();
 }
