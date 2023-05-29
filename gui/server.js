c18615d3f31833c4a488c7ed8f90f6c7b47f883d
mart-anthony-stark <martsalazar0@gmail.com>
2023-05-29 20:58:16 +0800

    Add: generate project

--- gui/server.js
+++ gui/server.js
@@ -3,11 +3,13 @@ import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 import path from "path";
 import { fileURLToPath } from "url";
+import generateCRUD from "../crud/index.js";
+import templates from "../src/templates.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 7000;
-const fastify = Fastify({ logger: true });
+const fastify = Fastify();
 
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, "public"),
@@ -24,8 +26,38 @@ fastify.listen({ port: PORT }, (err, address) => {
 });
 
 fastify.get("/", (req, reply) => {
-  //   exec(`ngenly -g crud expressjs post`);
+  // generateCRUD("expressjs", "new");
   reply.sendFile("index.html");
+});
+
+fastify.post("/generate-project", (req, reply) => {
+  const { library, projname } = req.body;
+  const templatesMap = {
+    expressjs: templates.javascript.express.none.link,
+    expressts: templates.typescript.express.none.link,
+    expressjs_mongo:
+      templates.javascript.express["mongodb (mongoose)"].auth.link,
+    expressts_mongo:
+      templates.typescript.express["mongodb (mongoose)"].auth.link,
+    fastifyjs: templates.javascript.fastify.none.link,
+    fastifyts: templates.typescript.fastify.none.link,
+    fastifyjs_mongo:
+      templates.javascript.fastify["mongodb (mongoose)"].auth.link,
+    fastifyts_mongo:
+      templates.typescript.fastify["mongodb (mongoose)"].auth.link,
+  };
+
+  exec(
+    `npx degit ${templatesMap[library]} ${projname}`,
+    (error, stdout, output) => {
+      if (error) {
+        reply.send(error);
+        console.log(error);
+      }
+      console.log("Generated Project");
+      reply.send({ msg: "Generated the project successfully" });
+    }
+  );
 });
 
 /**
