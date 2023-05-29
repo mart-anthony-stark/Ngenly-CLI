26b8fea74ecf5ca98ebb8306d917f2eebc0da991
mart-anthony-stark <martsalazar0@gmail.com>
2023-05-29 18:12:02 +0800

    Add: gui server

---
+++ gui/index.js
@@ -0,0 +1,33 @@
+import { exec } from "child_process";
+import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 7000;
+const fastify = Fastify({ logger: true });
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "public"),
+  prefix: "/public/", // optional: default '/'
+  //   constraints: { host: "example.com" }, // optional: default {}
+});
+fastify.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server running on ${address}`);
+  console.log(__dirname);
+});
+
+fastify.get("/", (req, reply) => {
+  //   exec(`ngenly -g crud expressjs post`);
+  reply.sendFile("index.html");
+});
+
+export const startGUIserver = () => {
+  exec("nodemon ./gui/index.js");
+};
